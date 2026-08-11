bcd42a86eecba82b2ad22fd90515f6b460857f47
Anas Muselli <72192110+Ness-elim@users.noreply.github.com>
2026-08-11 14:17:10 +0300

    Update online-config.js

--- online-config.js
+++ online-config.js
@@ -1,5 +1,5 @@
 // DeutschPath 4.1 public online configuration.
 // The Apps Script Web app URL is not a secret. Keep your old Sync key out of GitHub.
 window.DEUTSCHPATH_ONLINE_CONFIG = {
-  webAppUrl: 'PASTE_YOUR_EXISTING_APPS_SCRIPT_EXEC_URL_HERE'
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbzCthaYdljb7wHHLeIN7OXuCNvJ66DQLgG93zC705jJApIhhwkZFGZWytbD9ApbD3on/exec'
 };
