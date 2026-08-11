ad6dfff03d1ca50c13285afce65d39d8dcc4fd23
Anas Muselli <72192110+Ness-elim@users.noreply.github.com>
2026-08-11 14:33:22 +0300

    Delete online-config.js

--- online-config.js
+++
@@ -1,5 +0,0 @@
-// DeutschPath 4.1 public online configuration.
-// The Apps Script Web app URL is not a secret. Keep your old Sync key out of GitHub.
-window.DEUTSCHPATH_ONLINE_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbzCthaYdljb7wHHLeIN7OXuCNvJ66DQLgG93zC705jJApIhhwkZFGZWytbD9ApbD3on/exec'
-};
